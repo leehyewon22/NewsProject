d9084f0168bf9c30f819ca7c855a4371f9855c24
smhrd <smhrd@121.147.52.247>
2022-07-28 15:48:31 +0900

    계속 사라졌던 로그인 로그아웃 다시함. 사라지지않길

--- NewsProject/src/main/java/com/smhrd/controller/LogoutCon.java
+++ NewsProject/src/main/java/com/smhrd/controller/LogoutCon.java
@@ -2,11 +2,13 @@ package com.smhrd.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/LogoutCon")
 public class LogoutCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
